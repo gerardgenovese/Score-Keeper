0c4b2e479bb143b16d7add7360c1e97b4d95f6c7
Gerard <gmoneymaster24@yahoo.com>
2017-06-20 17:56:00 -0400

    Score Keeper
    Set a number to play to and then click a button for each player to reach
    that number.

---
+++ Score Keeper/scoreKeeper.js
@@ -0,0 +1,54 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input") //input[type=number] if more than 1 input
+var resetButton = document.querySelector("#reset");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+var winningScoreDisplay = document.querySelector("p span");
+
+p1Button.addEventListener("click", function(){
+    if(!gameOver) {
+    p1Score++;
+        if(p1Score === winningScore) {
+             p1Display.classList.add("winner");
+            gameOver = true;
+        }
+    }
+   p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+    if(!gameOver) {
+    p2Score++;
+        if(p2Score === winningScore){
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+    }
+    p2Display.textContent = p2Score;
+});
+
+resetButton.addEventListener("click", function(){
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+   winningScoreDisplay.textContent = numInput.value; //value gives us the value of the input
+   winningScore = Number(numInput.value); //Number turns winning score into a string
+   reset();
+            //you can also repplace numInput.value with this.value
+});
